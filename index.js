52e7f61fa4572d07c4a1f5ac9edb8d69a1bdbeda
Danniel <danniel.master@gmail.com>
2020-11-24 12:13:08 -0300

    fix updates

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 
 const express = require('express')
 const app = express()
-const path = require('path')
-
-app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.resolve('node_modules', 'bootstrap', 'dist')))
-
-app.use(cors())
-
-// Configurações da view com ejs
-app.set('views', path.resolve('src', 'views'))
-app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.send('Hello World!')
 })
 
 app.listen(process.env.PORT, () => {
